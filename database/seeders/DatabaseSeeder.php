882db4634cd06d433f2216b04baf277c8da09761
salsabila <salsaabila.1824@gmail.com>
2021-07-19 19:58:51 +0700

    kumpul tugas

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'danang',
+            'email' => 'danang.dev8@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
